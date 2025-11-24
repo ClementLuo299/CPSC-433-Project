2b9f4c621708be0cba82f1c20e791caee3efc061
Clement Luo <clementluo299@gmail.com>
2025-11-23 20:06:09 -0700

    added Slot.py

---
+++ Slot.py
@@ -0,0 +1,8 @@
+class Slot:
+    def __init__(self, day, time, classmax, classmin, almax, is_tutorial):
+        self.day = day
+        self.time = time
+        self.classmax = classmax
+        self.classmin = classmin
+        self.almax = almax
+        self.is_tutorial = is_tutorial
