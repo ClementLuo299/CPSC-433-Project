9ccde3d7efdcf1671e93c830a13d2a491ae469f6
Clement Luo <clementluo299@gmail.com>
2025-11-23 19:14:12 -0700

    made changes to main

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import sys
 
-text_file_path = sys.argv[1]
-integer_inputs = [int(x) for x in sys.argv[2:]]
+# check for the minimum amount of arguments
+if len(sys.argv) < 9:
+    print("not enough arguments given")
+    exit()
 
-# Read file
+text_file_path = sys.argv[1]
+
+# handle the integer inputs
+try:
+    integer_inputs = [int(x) for x in sys.argv[2:]]
+except ValueError:
+    print("arguments must be integers")
+    exit()
+
+# read text file
 try:
     with open(text_file_path, "r") as f:
         text_data = f.read()
 except Exception as e:
-    print(f"Error reading file: {e}")
+    exit()
 
+print("File contents:", text_data)
+print("Integers:", integer_inputs)
