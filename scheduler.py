4f752b349d64f75b410042fad6856e018bd209c9
Clement Luo <clementluo299@gmail.com>
2025-12-03 18:36:12 -0700

    added comments to scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@ from parser import parse_file
 from solver import solve
 
 def main():
+    #Define parser for command line arguments
     parser = argparse.ArgumentParser(description="University Course Scheduler")
     parser.add_argument("filename", help="Input file path")
     parser.add_argument("w_minfilled", type=float)
@@ -14,18 +15,23 @@ def main():
     parser.add_argument("pen_tutorialmin", type=float, help="Unused")
     parser.add_argument("pen_notpaired", type=float)
     parser.add_argument("pen_section", type=float)
-    
+
+    #Read command line
     args = parser.parse_args()
-    
+
+    #Parse input file
     print("Parsing input file...")
     problem = parse_file(args.filename)
     print(f"Parsed {len(problem.lectures)} lectures and {len(problem.tutorials)} tutorials.")
-    
+
+    #Collect numerical arguments
     weights = (args.w_minfilled, args.w_pref, args.w_pair, args.w_secdiff, args.pen_notpaired, args.pen_section)
-    
+
+    #Do the search
     print("Starting solver...")
     solution, cost = solve(problem, weights)
-    
+
+    #Print solution in proper format if a solution exists
     if solution:
         print(f"Eval-value: {int(cost)}")
         # Sort assignments: Alphabetical by Course ID
