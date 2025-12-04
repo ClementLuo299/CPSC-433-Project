21dedfa14e54ed66aa018aa7288cf1c391188cd1
Clement Luo <clementluo299@gmail.com>
2025-12-03 18:29:21 -0700

    deleted old code

--- Search.py
+++
@@ -1,6 +0,0 @@
-class Search:
-    @staticmethod
-    def perform_search(search_input):
-        #perform the search on the search_input object
-        #return the final assignment
-        pass
--- helper_classes/Slot.py
+++
@@ -1,8 +0,0 @@
-class Slot:
-    def __init__(self, day, time, classmax, classmin, almax, is_tutorial):
-        self.day = day
-        self.time = time
-        self.classmax = classmax
-        self.classmin = classmin
-        self.almax = almax
-        self.is_tutorial = is_tutorial
